cf5d3bbaa84bf89b3e7bb655b2617ddf88dc7d5d
Leaf Based <LB@Js-MacBook-Pro.local>
2019-10-20 01:45:22 -0600

    td-002: Layout changes

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { TodoService } from '../services/todo.service';
+import { IonReorderGroup } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +8,10 @@ import { TodoService } from '../services/todo.service';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page implements OnInit {
+
+  // items = [1, 2, 3, 4, 5];
+
+  @ViewChild(IonReorderGroup, null) reorderGroup: IonReorderGroup;
 
   todos: any = [];
 
@@ -16,6 +21,21 @@ export class Tab2Page implements OnInit {
 
   ngOnInit() {
     this.todos = this.todoService.fetchTodos();
+  }
+ 
+  doReorder(ev: any) {
+    // The `from` and `to` properties contain the index of the item
+    // when the drag started and ended, respectively
+    console.log('Dragged from index', ev.detail.from, 'to', ev.detail.to);
+
+    // Finish the reorder and position the item in the DOM based on
+    // where the gesture ended. This method can also be called directly
+    // by the reorder group
+    ev.detail.complete();
+  }
+
+  toggleReorderGroup() {
+    this.reorderGroup.disabled = !this.reorderGroup.disabled;
   }
 
 }
